cbaf11c29a519a12dc4ac0765918c56ea06b3c4c
avalonnadotti <nadottiavalon@gmail.com>
2024-03-18 22:19:15 -0300

    Commit final da aula 4

--- docker/node/entrypoint.sh
+++ docker/node/entrypoint.sh
@@ -6,4 +6,5 @@ echo "CodeLAB em ação!"
 npm run dev
 
 
+
 #tail -f /dev/null
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,16 @@
 import express from 'express'
-const app = express()
+import { Client } from 'pg'
+
+async function main() {
+    const app = express()
 const port = 3000
+
+const client = new Client()
+await client.connect()
+ 
+const res = await client.query('SELECT $1::text as message', ['DB Connection is OK!'])
+console.log(res.rows[0].message) // Hello world!
+await client.end()
 
 app.get('/', (req, res) => {
   res.send('Hello world!')
@@ -9,3 +19,6 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+}
+
+main()
